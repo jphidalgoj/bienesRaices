5b7dab080f7004e90b3ad420b8fdfd8258703af6
Jairo <jairohidalgo94@gmail.com>
2025-04-04 13:13:48 -0500

    Segundo comit

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -32,11 +32,10 @@ return [
     */
 
     'stores' => [
-
-        'array' => [
-            'driver' => 'array',
-            'serialize' => false,
-        ],
+    'file' => [
+        'driver' => 'file',
+        'path' => storage_path('framework/cache/data'),
+    ],
 
         'database' => [
             'driver' => 'database',
